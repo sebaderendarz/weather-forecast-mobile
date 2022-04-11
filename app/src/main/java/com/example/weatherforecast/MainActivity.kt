aadcc467d0ff903ddfb0294ff8bc73e74e59b3b8
SebastianDerendarz <Derendarz99@gmail.com>
2022-04-11 22:27:49 +0200

    Add settings fragment, base of other fragments and navigation to fragments

--- app/src/main/java/com/example/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforecast/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.weatherforecast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    val favouritesFragment = FavouritesFragment()
+    private val homeFragment = HomeFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, homeFragment)
+            commit()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -20,14 +28,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.homeItem) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, homeFragment)
+                addToBackStack(null)
+                commit()
+            }
             // redirect to home fragment
-            Toast.makeText(applicationContext, "Home was clicked", Toast.LENGTH_LONG).show()
+            // Toast.makeText(applicationContext, "Home was clicked", Toast.LENGTH_LONG).show()
         } else if (item.itemId == R.id.favouritesItem) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, favouritesFragment)
+                addToBackStack(null)
+                commit()
+            }
             // redirect to favourites fragment
-            Toast.makeText(applicationContext, "Favourites was clicked", Toast.LENGTH_LONG).show()
+            // Toast.makeText(applicationContext, "Favourites was clicked", Toast.LENGTH_LONG).show()
         } else if (item.itemId == R.id.settingItem){
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, settingsFragment)
+                addToBackStack(null)
+                commit()
+            }
             // redirect to settings fragment
-            Toast.makeText(applicationContext, "Settings was clicked", Toast.LENGTH_LONG).show()
+            // Toast.makeText(applicationContext, "Settings was clicked", Toast.LENGTH_LONG).show()
         }
         return super.onOptionsItemSelected(item);
     }
