c3e516d30795b3a2582c0503d9bc448c6ebc580e
SebastianDerendarz <Derendarz99@gmail.com>
2022-04-12 22:33:51 +0200

    'wip'

--- app/src/main/java/com/example/weatherforecast/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecast/HomeFragment.kt
@@ -5,22 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import com.example.weatherforecast.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-//    private var _binding = null;
-//    private val binding get() = _binding!!
+    private var _binding: FragmentHomeBinding? = null;
+    private val binding get() = _binding!!
+    private val weatherDetailsFragment: WeatherDetailsFragment = WeatherDetailsFragment()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null;
-//    }
+        binding.textInputLayout.setEndIconOnClickListener {
+            Toast.makeText(requireContext(), "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.textInputText.doOnTextChanged { text, _, _, _ ->
+            Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+        }
+
+        childFragmentManager.beginTransaction().apply {
+            replace(R.id.weatherDetailsLayout, weatherDetailsFragment)
+            commit()
+        }
+
+        return binding.root
+    }
 }
